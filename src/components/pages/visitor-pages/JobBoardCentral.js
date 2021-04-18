18005e028e99d0ebc79f419b85c83b3ac2eb2e64
Alex <alex.beciana@gmail.com>
2021-04-18 15:59:08 -0400

    Created job board central page

---
+++ src/components/pages/visitor-pages/JobBoardCentral.js
@@ -0,0 +1,11 @@
+import {React} from 'react';
+
+const JobBoardCentral = () => {
+    return (
+        <React.Fragment>
+            <h1>Job Boards</h1>
+        </React.Fragment>
+    )
+}
+
+export default JobBoardCentral
